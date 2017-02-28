4916b19251ec2d230e4cd7e6de76b87501d7c531
Felicity05 <arelysalvarezpadilla@Arelyss-MacBook-Air.local>
2017-02-28 11:08:17 -0500

    other change

--- myFifthAssignment/src/CommisionCalculator.java
+++ myFifthAssignment/src/CommisionCalculator.java
@@ -9,7 +9,7 @@
  * @author arelysalvarezpadilla
  */
 public class CommisionCalculator {
-    
+
     //instance variables 
     private String name;                    //sales person name
     private int totalSales;                 //total monthly sales
@@ -17,23 +17,18 @@ public class CommisionCalculator {
     private String rank;                       //rank of Apprentice, Junior, or Senior
     private String rankType;
     private double salesComis;
-    
-    
-    public CommisionCalculator(String name, int totalSales, int yearsServ, String rank){
+
+    public CommisionCalculator(String name, int totalSales, int yearsServ, String rank) {
         this.name = name;
         this.totalSales = totalSales;
         this.yearsServ = yearsServ;
         this.rank = rank;
     }
-    
-    
-     //setting the rank 
-    public void setRank(String input)
-    {
-        double rankInc = 0;
-        switch(input)
-        {
-            case "1": 
+
+    //setting the rank 
+    public void setRank(String input) {
+        switch (input) {
+            case "1":
                 rankType = "Apprentice";
                 break;
             case "2":
@@ -46,89 +41,83 @@ public class CommisionCalculator {
                 rankType = "ERROR, you have to select from 1 to 3";
                 break;
         }
-     
+
     }
-    
-    public String getRank()
-    {
+
+    public String getRank() {
         return rankType;
     }
+
+    
+    //printing the data
+    /*public String getData()
+    {
+       String data = "The name of the sales person is " + this.name
+               + "\n\nThe amount of years with the company are: " + yearsServ + " years"
+               + "\n\nFor rank: " + rank + " The rank type is: " + rankType;
+              // System.out.printf("\nThe total montly sales is: $%,d.00", totalSales);
+              String sales = String.format("\nThe total montly sales is: $%,d.00", totalSales);
+        return data;
+    }*/
     
     //making all the calculations 
-    public void computeComission(double totalSales)
-    {
+    public double computeComission() {
         double comiss = 0;
-        if (totalSales > 400000)
-        {
+        if (totalSales > 400000) {
             salesComis = 0.02;
-            comiss =  salesComis * totalSales;
-        }
-        else if (totalSales > 300000 )
-        {
+            comiss = salesComis * totalSales;
+        } else if (totalSales > 300000) {
             salesComis = 0.0175;
-            comiss =  salesComis * totalSales;
-        }
-        else if (totalSales > 200000) {
+            comiss = salesComis * totalSales;
+        } else if (totalSales > 200000) {
             salesComis = 0.015;
-            comiss =  salesComis * totalSales;
-        }      
-        else if (totalSales >= 100000)
-        {
+            comiss = salesComis * totalSales;
+        } else if (totalSales >= 100000) {
             salesComis = 0.01;
-            comiss =  salesComis * totalSales;
-        }
-        else
-        {
-            System.out.println("\nLo siento, you haven't satisfied the sales goal" 
+            comiss = salesComis * totalSales;
+        } else {
+            System.out.println("\nLo siento, you haven't satisfied the sales goal"
                     + "so you don't have any commission");
         }
-        
+
         System.out.println("\nThe commission assigned per sales is: $" + salesComis);
         System.out.println("\nThe commission is: $" + comiss);
-        
+
         //commission regarding to years with the company
         double yearComiss;
-        if (yearsServ >= 10)
-        {
-            yearComiss = 0.01 * totalSales * yearsServ; 
+        if (yearsServ >= 10) {
+            yearComiss = 0.01 * totalSales * yearsServ;
+        } else {
+            yearComiss = 0.01 * 0.1 * totalSales * yearsServ;
         }
-        else 
-        {
-            yearComiss = 0.01 * 0.1 * totalSales * yearsServ; 
-        }
-        
+
         System.out.println("\nAccording to the years with the company you also have $"
-                             + yearComiss + " additional commission");
-        
+                + yearComiss + " additional commission");
+
         //according to the rank
         double rankInc = 0;
-        switch(rank)
-        {
-            case "1": 
+        switch (rank) {
+            case "1":
                 System.out.println("\nSorry, you don't receive rank incentive");
                 break;
             case "2":
                 rankInc = 0.001 * totalSales;
                 System.out.println("\nYour incentive according to the rank is: $"
-                            + rankInc);
+                        + rankInc);
                 break;
             case "3":
                 rankInc = 0.002 * totalSales;
                 System.out.println("\nYour incentive according to the rank is: $"
-                            + rankInc);
+                        + rankInc);
                 break;
             default:
                 rankType = "\nERROR, you have to select rank from 1 to 3";
                 break;
         }
-        
-        System.out.println("\nYour total commission would be: $" + (comiss + yearComiss + rankInc));
-           
-    }
-    
 
-    
-    
-    
-    
+        System.out.println("\nYour total commission would be: $" + (comiss + yearComiss + rankInc));
+        return 0;
+
+    }
+
 }
--- myFifthAssignment/src/CommisionCalculatorTest.java
+++ myFifthAssignment/src/CommisionCalculatorTest.java
@@ -1,5 +1,4 @@
 
-import java.io.PrintStream;
 import javax.swing.JOptionPane;
 
 /*
@@ -7,7 +6,6 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author arelysalvarezpadilla
@@ -18,55 +16,45 @@ public class CommisionCalculatorTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
         int totalSales;
         int yearsService;
-        String name;
-        String rank;
         String input;
-        
+
         //getting all the data from the user 
-        name = JOptionPane.showInputDialog("Insert the sales associate name");
-        
+        String name = JOptionPane.showInputDialog("Insert the sales associate name");
+
         input = JOptionPane.showInputDialog("Insert the amount of sales in a month");
-        
+
         totalSales = Integer.parseInt(input);
-        
+
         input = JOptionPane.showInputDialog("Insert the years of services");
-        
+
         yearsService = Integer.parseInt(input);
-        
-        rank = JOptionPane.showInputDialog("Insert the rank");
-        
-        
-       
+
+        String rank = JOptionPane.showInputDialog("Insert the rank");
+
         //creating the calculator
         CommisionCalculator myCalculator = new CommisionCalculator(input, totalSales, yearsService, rank);
-       
-       
+
         //calling the rank 
-         myCalculator.setRank(rank);  
+        myCalculator.setRank(rank);
         
+        //getting the data
+       // System.out.println(myCalculator.getData());
         
-        
+
         System.out.println("The name of the sales person is " + name);
-                 
+
         System.out.printf("\nThe total montly sales is: $%,d.00", totalSales);
-                
-                                     
+
         System.out.println("\n\nThe amount of years with the company are: " + yearsService + " years"
-                 + "\n\nThe rank is: " + myCalculator.getRank());       
-         
-         
-          
+                + "\n\nFor rank: " + rank + " The rank type is: " + myCalculator.getRank());
         
         
         //computing all the commissions
-         myCalculator.computeComission(totalSales);
-        
-        
-        
+        myCalculator.computeComission();
+
     }
-    
+
 }
